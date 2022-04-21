8390c368ce250c4d97d31fba69cab4bb30e8dd06
le0li9ht <34128579+le0li9ht@users.noreply.github.com>
2022-04-21 18:41:49 +0530

    Update Telnetcheck.ps1

--- Telnetcheck.ps1
+++ Telnetcheck.ps1
@@ -1,6 +1,7 @@
-$path=args[1]
-$ipipchecklist = Get-Content -Path $path
+#arguments
 $Port=$args[0]
+$path=$args[1]
+#validate whether the arguments are given properly
 if (($Port -eq $null) -or ($path -eq $null)) {
         Write-Output "Please pass port number and Iplist file path as arguments
         `nRunas below:
@@ -8,20 +9,29 @@ if (($Port -eq $null) -or ($path -eq $null)) {
         `n If you want to export output to a file then run as `nTelnetcheck.ps1 <portnumber> <filepath> >output.txt"
         exit          
 }
+#read the IPlistfile
+$ipchecklist = Get-Content -Path $path
+#run a loop to get each IP from the given file
 ForEach ($file in $ipchecklist) {
+#trim whitespaces from the entry
 $file=$file.Trim()
+#if the entry is not null
+if( $file -ne $null) {
+#Create Socket Object
 $Socket = New-Object Net.Sockets.TcpClient
-$ErrorActionPreferenc = 'SilentlyContinue'
+#SilentlyContinue: No effect. The error message isn't displayed and execution continues without interruption.
+$ErrorActionPreference = 'SilentlyContinue'
+#Connect to the port
 $Socket.Connect($file, $Port)
-$ErrorActionPreference = 'Continue'
-
+#Continue: (Default) Displays the error message and continues executing.
+$ErrorActionPreference = 'SilentlyContinue'
 if ($Socket.Connected) {
-   "For ${file}: Port $Port is open"
+   "For ${file} Port $Port is open"
     $Socket.Close()
 }
 else {
-            "${file}: Port $Port is closed or filtered" 
-
+            "For ${file} Port $Port is closed or filtered" 
         }
+$Socket.close()
 $Socket = $null
-}
+}}
