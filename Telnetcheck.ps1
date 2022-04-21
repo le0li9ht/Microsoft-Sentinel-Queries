ce7270bfd365c88e80ff9a22005ec96817c2a68d
le0li9ht <34128579+le0li9ht@users.noreply.github.com>
2022-04-21 13:40:35 +0530

    Add files via upload

---
+++ Telnetcheck.ps1
@@ -0,0 +1,26 @@
+﻿$ipipchecklist = Get-Content -Path C:\Users\MBUSER\Desktop\auto\telnet.txt
+$Port=$args[0]
+if ($Port -eq $null){
+        Write-Output "Please pass port number as argument
+        `nRunas below:
+        Telnetcheck.ps1 <portnumber> 
+        `n If you want to export output to a file then run as `nTelnetcheck.ps1 <portnumber> >output.txt"
+        exit          
+}
+ForEach ($file in $ipchecklist) {
+$file=$file.Trim()
+$Socket = New-Object Net.Sockets.TcpClient
+$ErrorActionPreferenc = 'SilentlyContinue'
+$Socket.Connect($file, $Port)
+$ErrorActionPreference = 'Continue'
+
+if ($Socket.Connected) {
+   "For ${file}: Port $Port is open"
+    $Socket.Close()
+}
+else {
+            "${file}: Port $Port is closed or filtered" 
+
+        }
+$Socket = $null
+}
